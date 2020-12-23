bbdd96dc9dfb5387c95f1f42a4e6f97b0f5420e6
Sunil <61093092+gunducoder@users.noreply.github.com>
2020-12-23 13:55:26 -0600

    Launching chrome.exe programatically
    Launching chrome.exe programatically

--- Form1.cs
+++ Form1.cs
@@ -6,6 +6,7 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Configuration;
 using System.Data;
+using System.Diagnostics;
 using System.Drawing;
 using System.Linq;
 using System.Text;
@@ -24,9 +25,16 @@ namespace WebClick
 
             var chromeOptions = new ChromeOptions();
             chromeOptions.DebuggerAddress = "localhost:9014";
-            //  chromeOptions.AddAdditionalCapability("debuggerAddress", "localhost:9014");
-            driver  = new ChromeDriver(chromeOptions);
-            //driver.Navigate().GoToUrl(ConfigurationManager.AppSettings["url"]);
+            try
+            {
+                driver = new ChromeDriver(chromeOptions);
+            }catch(Exception e)
+            {
+                //Open the browser , if it not opened
+                LaunchChrome();
+                driver = new ChromeDriver(chromeOptions);
+            }
+
             WebDriverWait wait = new WebDriverWait(driver, System.TimeSpan.FromSeconds(15));
 
         }
@@ -50,6 +58,22 @@ namespace WebClick
         {
             var buyElement = driver.FindElement(By.XPath("html/body/div[3]/div[3]/div/div/div[2]/div[3]/div[2]/form/div[2]/div[1]/div[2]/div[3]/a"));
             buyElement.Click();
+        }
+
+        private void LaunchChrome()
+        {
+            Process process = new Process();
+            process.StartInfo = new ProcessStartInfo()
+            {
+                WindowStyle = ProcessWindowStyle.Hidden,
+                UseShellExecute = false,
+                RedirectStandardOutput = true,
+                CreateNoWindow = true,
+                StandardOutputEncoding = Encoding.UTF8,
+                FileName = Environment.GetEnvironmentVariable("ProgramFiles(x86)") + @"\Google\Chrome\Application\chrome.exe",
+                Arguments = ConfigurationManager.AppSettings["url"] + " -remote-debugging-port=9014 --user-data-dir=" + @"C:\temp\"
+            };
+            process.Start();
         }
     }
 }
